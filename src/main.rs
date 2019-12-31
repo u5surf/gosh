5ec24223a3ec38e8c21e13c5d183a9c55dca3c2d
Sean Hellum <seanhellum45@gmail.com>
2019-12-31 22:48:23 +0000

    add basic command execution

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
-use std::io::{stdin,stdout,Write};
+use std::io::{stdin, stdout, Write};
+use subprocess::{Exec,Redirection};
 fn main() {
     loop {
         let mut command = String::new();
         print!("\x1b[0;32mgosh \x1b[0;34mλ \x1b[0m ");
         stdout().flush().unwrap();
         stdin().read_line(&mut command).unwrap();
+        let command = Exec::cmd(command.trim())
+            .stdout(Redirection::Pipe)
+            .capture();
+        let out = match command {
+            Ok(n) => n.stdout_str(),
+            Err(_) => {
+                println!("Error");
+                continue;
+            },
+        };
+        println!("{}", out);
     }
 }
